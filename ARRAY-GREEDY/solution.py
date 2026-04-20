3ee941bca220fc8f493485ccd653cfbdc8912996
Alanvarghese-sbc <alanvarghese458@gmail.com>
2026-04-20 23:20:47 +0530

    code update

--- ARRAY-GREEDY/solution.py
+++ ARRAY-GREEDY/solution.py
@@ -1,14 +1,21 @@
-# n = int(input())
+n = int(input())
 arr = list(map(int, input().split()))
 x = int(input())
 
+if n <= 0:
+    print(0)
+    exit()
+
 arr.sort()
+
 count = 0
-sum = 0
+current_sum = 0
+
 for i in arr:
-    if sum + i <= x:
-        sum += i
+    if current_sum + i <= x:
+        current_sum += i
         count += 1
     else:
         break
+
 print(count)
